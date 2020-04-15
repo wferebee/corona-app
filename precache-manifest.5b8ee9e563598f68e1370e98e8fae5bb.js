9ebcc6aadbe0851d7e9fe23bf48d7013701dc599
DP3PO <williampferebee@gmail.com>
2020-04-14 22:38:31 -0400

    Updates

---
+++ precache-manifest.5b8ee9e563598f68e1370e98e8fae5bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b149a5c0e843bad2b3b0291e969537",
+    "url": "/corona-app/index.html"
+  },
+  {
+    "revision": "673fd2ac51854fc42234",
+    "url": "/corona-app/static/css/main.d5b381f6.chunk.css"
+  },
+  {
+    "revision": "e42f726b0e9905ee5a48",
+    "url": "/corona-app/static/js/2.db7dfa69.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/corona-app/static/js/2.db7dfa69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "673fd2ac51854fc42234",
+    "url": "/corona-app/static/js/main.670e2624.chunk.js"
+  },
+  {
+    "revision": "a74ea87a626e436384c4",
+    "url": "/corona-app/static/js/runtime-main.785bd798.js"
+  }
+]);
